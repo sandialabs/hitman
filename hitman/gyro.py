9ac70660f312298a206ac56f283e97e9ce28a4bc
Mike Walker <mrwalke@sandia.gov>
2026-01-28 11:23:07 -0700

    whitespace.

--- hitman/gyro.py
+++ hitman/gyro.py
@@ -8,7 +8,7 @@ Consider the relationships
     \dot{\phi} &= \left(J_\phi^r\right)^{-1} \omega^r\\
     \Delta R(t) &= \exp\big(-\phi(t-\tau)\big)\exp\big(\phi(t)\big) \\
     \Delta \theta &= \int_{t-\tau}^t \omega^r(s) ds
-    
+
 
 If :math:`\omega` is defined as a polynomial, then :math:`\Delta \theta` is available
 directly. However, :math:`\phi` must be solved numerically. Conversely, if :math:`\phi`
